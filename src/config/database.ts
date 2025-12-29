18ec4326da20c85ee794da64b1b21f664e32a948
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-12-29 22:13:48 +0530

    all finished

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,6 +6,13 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, 
+  max: 1,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 export const db = drizzle(pool);
+
+setInterval(() => {
+  db.execute(`SELECT 1`);
+}, 5000); 
